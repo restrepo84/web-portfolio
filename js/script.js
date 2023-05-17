258481e185cafa6bd95c4464067c5d05f0bece9f
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-05-17 16:11:02 -0600

    add contact submit
    now ill be able to get all the info from people

--- js/script.js
+++ js/script.js
@@ -6,3 +6,16 @@ hamburger.addEventListener("click", function () {
   this.classList.toggle("click");
   navlist.classList.toggle("open");
 });
+
+const sendEmail = (event) => {
+  event.preventDefault();
+  emailjs.sendForm("service_t6arflv", "template_6jh0dne", "#form").then(
+    function (response) {
+      console.log("SUCCESS!", response.status, response.text);
+    },
+    function (error) {
+      console.log("FAILED...", error);
+    }
+  );
+};
+document.querySelector("#form").addEventListener("submit", sendEmail);
